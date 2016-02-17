913c3fb0b195f6b277c5bd1389ed83d522ad0499
evesgf <evesgf@163.com>
2016-02-18 00:23:13 +0800

    添加 拉勾网 抓取方法

--- DEV/LittleBot/LittleBot/Common/HttpHelper.cs
+++ DEV/LittleBot/LittleBot/Common/HttpHelper.cs
@@ -95,6 +95,18 @@ namespace ZHBot
             httpWebResponse.Close();
             return html;
         }
+
+        public static string UrlEncode(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] byStr = System.Text.Encoding.UTF8.GetBytes(str); //默认是System.Text.Encoding.Default.GetBytes(str)
+            for (int i = 0; i < byStr.Length; i++)
+            {
+                sb.Append(@"%" + Convert.ToString(byStr[i], 16));
+            }
+
+            return (sb.ToString());
+        }
     }
 
     public class HttpHeader
@@ -119,4 +131,5 @@ namespace ZHBot
 
         public int maxTry { get; set; }
     }
+
 }
--- DEV/LittleBot/LittleBot/Program.cs
+++ DEV/LittleBot/LittleBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using LittleBot.Service;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,19 @@ namespace LittleBot
     {
         static void Main(string[] args)
         {
-            ZHService zhs = new ZHService();
-            zhs.StartZHBot();
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+
+            //ZHService zhs = new ZHService();
+            //zhs.StartZHBot();
+
+            LGService lgService=new LGService();
+            lgService.StartZHBot();
+
+            //输出时间
+            sw2.Stop();
+            TimeSpan ts2 = sw2.Elapsed;
+            Console.WriteLine("总共花费{0}ms.", ts2.TotalMilliseconds);
 
             Console.ReadKey(true);
         }
